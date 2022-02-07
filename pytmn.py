5d52332d7d4940d68554d97979b6bcce349d547f
lxenard <laura.xenard@pasteur.fr>
2022-02-07 14:55:39 +0100

    Add stub code files

---
+++ pytmn.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Importation of ImageJ TrackMate results (XML file) as a Python network
+for tracks analysis.
+"""
+
+__author__ = "Laura Xénard"
+__contact__ = "laura.xenard@pasteur.fr"
+__copyright__ = "GNU GPLv3"
+__date__ = "2022-02-07"
+__version__ = "0.1"
+
+import xml.etree.ElementTree as et
+
+import networkx
+
+
+if __name__ == " __main__ ":
+    pass
---
+++ test_pytmn.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Unit test for pyTMn.
+"""
+
+import pytest
+
+import pytmn
