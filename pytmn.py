e4fbb3947e80ff9a98a181d186bd8af50b6ef55e
lxenard <laura.xenard@pasteur.fr>
2022-02-07 17:12:32 +0100

    Add parsing of model info

--- pytmn.py
+++ pytmn.py
@@ -11,10 +11,54 @@ __copyright__ = "GNU GPLv3"
 __date__ = "2022-02-07"
 __version__ = "0.1"
 
+import argparse
 import xml.etree.ElementTree as et
 
-import networkx
+import networkx as nx
 
 
-if __name__ == " __main__ ":
-    pass
+def add_model_info(graph, element):
+    """Add graph attributes from an XML element.
+
+    Args:
+        graph (nx.Graph): Graph on which to add attributes.
+        element (et.Element): Element holding the information to be added.
+
+    Returns:
+        nx.Graph: The updated graph.
+    """
+    
+    for k, v in element.attrib.items():
+        graph.graph[k] = v
+    return graph
+   
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("xml", help="path of the XML file to process")
+    args = parser.parse_args()
+
+    # Creation of a graph that will hold all the tracks described
+    # in the XML file. This means that if there's more than one track,
+    # the resulting graph will be disconnected.
+    graph = nx.Graph()
+
+    # So as not to load the entire XML file into memory, we're using an
+    # iterator to browse over the tags one by one.
+    # The events 'start' and 'end' correspond respectively to the opening 
+    # and the closing of the considered tag.
+    it = et.iterparse(args.xml, events=['start', 'end'])
+
+    for event, element in it:
+
+        # Adding the model information as graph attributes.
+        if element.tag == 'Model' and event == 'start':
+            graph = add_model_info(graph, element)
+
+        
+            
+
+    print(graph)
+    print(graph.graph)
+            
