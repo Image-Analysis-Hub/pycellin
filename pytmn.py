65cacdc2a29f99c7d6bb63cfbe6121327111748e
lxenard <laura.xenard@pasteur.fr>
2022-02-08 18:45:45 +0100

    Chop big graph by tracks and add tracks attributes

--- pytmn.py
+++ pytmn.py
@@ -47,9 +47,8 @@ def add_all_nodes(graph, iterator, ancestor):
         ancestor (ET.Element): Element encompassing the information to add.
     """
     event, element = next(iterator)
-
     while (event, element) != ('end', ancestor):
-        event, element = next(iterator)
+        event, element = next(iterator)          
         if event == 'start' and element.tag == 'Spot': 
             node_id = element.attrib.pop('ID')
             graph.add_node(node_for_adding=node_id, attr=element.attrib)
@@ -65,16 +64,69 @@ def add_all_edges(graph, iterator, ancestor):
         graph (nx.Graph): Graph on which to add edges.
         iterator (ET.iterparse): XML element iterator.
         ancestor (ET.Element): Element encompassing the information to add.
+
+    Returns:
+        list(dict): A dictionary of attributes for every track.
     """
+    tracks_attributes = []
     event, element = next(iterator)
+    # Initialisation of current track information.
+    if element.tag == 'Track' and event == 'start':
+        # This condition is a bit of an overkill since the XML structure
+        # SHOULD stay the same but better safe than sorry.
+        tracks_attributes.append(element.attrib)
+        current_track_id = element.attrib['TRACK_ID']
+
     while (event, element) != ('end', ancestor):
         event, element = next(iterator)
+
+        # Saving the current track information.
+        if element.tag == 'Track' and event == 'start':
+            tracks_attributes.append(element.attrib)
+            current_track_id = element.attrib['TRACK_ID']
+
+        # Edge creation.
         if event == 'start' and element.tag == 'Edge': 
             entry_node = element.attrib.pop('SPOT_SOURCE_ID')
             exit_node = element.attrib.pop('SPOT_TARGET_ID')
             graph.add_edge(entry_node, exit_node, attr=element.attrib)
             element.clear()
-               
+            # Adding the current track ID to the nodes of the newly created
+            # edge. This will be useful later to filter nodes by track and
+            # add the saved tracks attributes (as returned by this method).
+            graph.nodes[entry_node]['TRACK_ID'] = current_track_id
+            graph.nodes[exit_node]['TRACK_ID'] = current_track_id
+            
+    return tracks_attributes
+
+
+def add_tracks_info(graphs, tracks_attributes):
+    """Add track attributes to each corresponding graph.
+
+    Args:
+        graphs (nx.Graph): List of graphs to update.
+        tracks_attributes (list(dict)): Dictionaries of tracks attributes.
+
+    Returns:
+        list(nx.Graph): List of the updated graphs.
+    """
+    updated_graphs = []
+    for i, graph in enumerate(graphs):
+
+        # Finding the dict of attributes matching the track.
+        tmp = set(t for n, t in graph.nodes(data='TRACK_ID'))
+        assert len(tmp) == 1, ('Impossible state: several IDs for one track')
+        current_track_id = list(tmp)[0]
+        current_track_attr = [d_attr for d_attr in tracks_attributes 
+                              if d_attr['TRACK_ID'] == current_track_id][0]
+
+        # Adding the attributes to the graph.
+        for k, v in current_track_attr.items():
+            graph.graph[k] = v
+        updated_graphs.append(graph)
+
+    return updated_graphs
+             
 
 if __name__ == "__main__":
 
@@ -112,9 +164,8 @@ if __name__ == "__main__":
             root.clear()
 
         # Adding the tracks as edges.
-        # TODO: add Tracks attributes somewhere (subgraphs?).
         if element.tag == 'AllTracks' and event == 'start':
-            add_all_edges(graph, it, element)
+            tracks_attributes = add_all_edges(graph, it, element)
             root.clear()
 
             # Removal of filtered spots / nodes.
@@ -122,12 +173,25 @@ if __name__ == "__main__":
                 # Those nodes belong to no tracks: they have a degree of 0.
                 lone_nodes = [n for n, d in graph.degree if d == 0]
                 graph.remove_nodes_from(lone_nodes)
-            
+
+        # Filtering out tracks and adding tracks attribute.
+        if element.tag == 'FilteredTracks' and event == 'start':
+
+            # Subgraphs creation.
+            # One subgraph is created per track, so each subgraph is
+            # a connected component of `graph`.
+            graphs = [graph.subgraph(c).copy() 
+                      for c in nx.connected_components(graph)]
+            del graph  # Redondant with the subgraphs.
+
+            # Adding the tracks attributes as graphs attributes.
+            graphs = add_tracks_info(graphs, tracks_attributes)
+
         # TODO: add Settings as graph attributes. Is everything relevant?
 
-
-    print(graph)
-    print(nx.number_connected_components(graph))    
-
-    nx.draw(graph, with_labels=True, font_weight='bold')
-    plt.show()
+    # Very basic visualisation.
+    for graph in graphs:
+        print(graph)
+        print(graph.graph)
+        nx.draw(graph, with_labels=True, font_weight='bold')
+        plt.show()
