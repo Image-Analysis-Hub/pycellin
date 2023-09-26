298502df7524f85ab231ee2b7c33ceca11f108c1
lxenard <laura.xenard@pasteur.fr>
2023-09-26 20:07:17 +0200

    Add basic setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name="pycellin",
+    version="0.2",
+    description="Python Distribution Utilities",
+    author="Laura Xénard",
+    author_email="laura.xenard@pasteur.fr",
+    packages=["pycellin", "pycellin.trackmate_io"],
+)
