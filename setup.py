88ec2a8c937c208c1c78be0f29a749612a6067b0
lxenard <laura.xenard@pasteur.fr>
2023-11-13 18:11:11 +0100

    Change license to BSD 3-clause

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 setup(
     name="pycellin",
     version="0.2",
-    licence="GNU GPLv3",
+    licence="BSD-3-Clause",
     description="Conversion and analysis of TrackMate tracks as networkX directed graphs",
     author="Laura Xénard",
     author_email="laura.xenard@pasteur.fr",
