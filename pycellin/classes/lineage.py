d38d9c903ae956babd2f68b9d963069df10a1f3a
Laura Xénard <laura.xenard@pasteur.fr>
2024-07-16 10:01:47 +0200

    Attempt to plot lineage with Plotly

--- pycellin/classes/lineage.py
+++ pycellin/classes/lineage.py
@@ -4,8 +4,10 @@
 from abc import ABCMeta, abstractmethod
 from typing import Optional
 
+from igraph import Graph, EdgeSeq
 import matplotlib.pyplot as plt
 import networkx as nx
+import plotly.graph_objects as go
 
 
 class Lineage(nx.DiGraph, metaclass=ABCMeta):
@@ -184,6 +186,7 @@ class Lineage(nx.DiGraph, metaclass=ABCMeta):
         # https://python.igraph.org/en/stable/
         # https://python.igraph.org/en/stable/generation.html#from-external-libraries
         # Or maybe I can let the user choose between pygraphviz and plotly?
+
         pos = nx.drawing.nx_agraph.graphviz_layout(self, prog="dot")
         # The computed positions do not temporally align the nodes, so we force
         # the y position to be the frame number.
@@ -192,6 +195,105 @@ class Lineage(nx.DiGraph, metaclass=ABCMeta):
             plt.figure(figsize=figsize)
         nx.draw(self, pos, with_labels=True, arrows=True, font_weight="bold")
         plt.show()
+
+    def plot_with_plotly(self):
+        """
+        Plot the lineage as a tree using Plotly.
+        """
+
+        def make_annotations(pos, labels, font_size=10, font_color="rgb(250,250,250)"):
+            L = len(pos)
+            if len(labels) != L:
+                raise ValueError("The lists pos and text must have the same len")
+            annotations = []
+            for k in range(L):
+                annotations.append(
+                    dict(
+                        text=labels[
+                            k
+                        ],  # or replace labels with a different list for the text within the circle
+                        x=pos[k][0],
+                        y=2 * M - position[k][1],
+                        xref="x1",
+                        yref="y1",
+                        font=dict(color=font_color, size=font_size),
+                        showarrow=False,
+                    )
+                )
+            return annotations
+
+        # Conversion of the lineage/graph from networkx to igraph.
+        G = Graph.from_networkx(self)
+        # print(g)
+
+        nr_vertices = G.vcount()
+        v_label = G.vs["ID"]
+        lay = G.layout("rt")  # plot tree
+
+        position = {k: lay[k] for k in range(nr_vertices)}
+        Y = [lay[k][1] for k in range(nr_vertices)]
+        # Y = [G.vs["ID"]]
+        M = max(Y)
+
+        es = EdgeSeq(G)  # sequence of edges
+        E = [e.tuple for e in G.es]  # list of edges
+
+        L = len(position)
+        Xn = [position[k][0] for k in range(L)]
+        Yn = [2 * M - position[k][1] for k in range(L)]
+        Xe = []
+        Ye = []
+        for edge in E:
+            Xe += [position[edge[0]][0], position[edge[1]][0], None]
+            Ye += [2 * M - position[edge[0]][1], 2 * M - position[edge[1]][1], None]
+
+        fig = go.Figure()
+        fig.add_trace(
+            go.Scatter(
+                x=Xe,
+                y=Ye,
+                mode="lines",
+                line=dict(color="rgb(210,210,210)", width=1),
+                hoverinfo="none",
+            )
+        )
+        fig.add_trace(
+            go.Scatter(
+                x=Xn,
+                y=Yn,
+                mode="markers",
+                name="bla",
+                marker=dict(
+                    symbol="circle",
+                    size=18,
+                    color="#6175c1",  #'#DB4551',
+                    line=dict(color="rgb(50,50,50)", width=1),
+                ),
+                text=v_label,
+                hoverinfo="text",
+                opacity=0.8,
+            )
+        )
+
+        axis = dict(
+            showline=False,  # hide axis line, grid, ticklabels and  title
+            zeroline=False,
+            showgrid=False,
+            showticklabels=False,
+        )
+
+        fig.update_layout(
+            title="Title",
+            annotations=make_annotations(position, v_label),
+            font_size=5,
+            showlegend=False,
+            xaxis=axis,
+            yaxis=axis,
+            margin=dict(l=40, r=40, b=85, t=100),
+            hovermode="closest",
+            plot_bgcolor="rgb(248,248,248)",
+        )
+        fig.show()
 
 
 class CellLineage(Lineage):
