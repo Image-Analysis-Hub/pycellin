caf3cb6f0671be3acbd21cfaa2e8e0c2a60f54b5
Mini-Miette <laura.xenard@protonmail.com>
2024-07-24 20:27:38 +0200

    Notes for CTC exporter

--- pycellin/io/cell_tracking_challenge/exporter.py
+++ pycellin/io/cell_tracking_challenge/exporter.py
@@ -1,2 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+# Remove all features
+# Ensure that there is no lineage_ID of 0, since in CTC format it means
+# that a CTC track does not have a parent track.
