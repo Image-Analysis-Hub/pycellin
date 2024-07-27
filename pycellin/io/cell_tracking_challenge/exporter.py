24ac2efb9ed0044d906d1442d1f33defcab643c0
Mini-Miette <laura.xenard@protonmail.com>
2024-07-27 12:41:59 +0200

    Cleaning and docstrings

--- pycellin/io/cell_tracking_challenge/exporter.py
+++ pycellin/io/cell_tracking_challenge/exporter.py
@@ -1,28 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import itertools
-import math
-from typing import Any, Union
 
-from lxml import etree as ET
-import networkx as nx
-
-from pycellin.classes.model import Model
-from pycellin.classes.feature import FeaturesDeclaration, Feature
-from pycellin.classes.data import CoreData
 from pycellin.classes.lineage import CellLineage
 from pycellin.io.trackmate.loader import load_TrackMate_XML
 
-# TODO: Need to check with TM but I think I need to scrap the lineage_ID.
-# It is not used in the same way as TM and Pycellin in the CTC format.
-# https://public.celltrackingchallenge.net/documents/Naming%20and%20file%20content%20conventions.pdf
-# https://imagej.net/plugins/trackmate/actions/trackmate-ctc-exporter
-
 
 def sort_nodes_by_frame(
-    lineage: CellLineage, nodes: list[int]
-) -> tuple[list[int], list[int]]:
+    lineage: CellLineage,
+    nodes: list[int],
+) -> list[int]:
     """
     Sort the nodes by ascending frame.
 
@@ -35,17 +22,18 @@ def sort_nodes_by_frame(
 
     Returns
     -------
-    tuple[list[int], list[int]]
-        A tuple containing the ordered nodes and their corresponding frames.
+    list[int]
+        A list of nodes ordered by ascending frame.
     """
     sorted_list = [(node, lineage.nodes[node]["frame"]) for node in nodes]
     sorted_list.sort(key=lambda x: x[1])
-    nodes = [node for node, frame in sorted_list]
-    frames = [frame for node, frame in sorted_list]
-    return nodes, frames
+    return [node for node, _ in sorted_list]
 
 
-def _find_gaps(lineage: CellLineage, sorted_nodes: list[int]) -> list[tuple[int, int]]:
+def _find_gaps(
+    lineage: CellLineage,
+    sorted_nodes: list[int],
+) -> list[tuple[int, int]]:
     """
     Find the temporal gaps in an ordered list of nodes.
 
@@ -67,18 +55,40 @@ def _find_gaps(lineage: CellLineage, sorted_nodes: list[int]) -> list[tuple[int,
     for i in range(len(sorted_nodes) - 1):
         frame = lineage.nodes[sorted_nodes[i]]["frame"]
         next_frame = lineage.nodes[sorted_nodes[i + 1]]["frame"]
-        # frame, node = sorted_nodes[i]
-        # next_frame, next_node = sorted_nodes[i + 1]
         if next_frame - frame > 1:
             gap_nodes.append((sorted_nodes[i], sorted_nodes[i + 1]))
-            # gap_nodes.append(sorted_nodes[i + 1])
 
     return gap_nodes
 
 
 def _add_track(
-    lineage, sorted_nodes, ctc_tracks, node_to_parent_track, current_track_label
+    lineage: CellLineage,
+    sorted_nodes: list[int],
+    ctc_tracks: dict[int, dict[str, int]],
+    node_to_parent_track: dict[int, int],
+    current_track_label: int,
 ):
+    """
+    Add a CTC track to the CTC output.
+
+    Parameters
+    ----------
+    lineage : CellLineage
+        The lineage object to which the nodes belongs.
+    sorted_nodes : list[int]
+        A list of nodes ordered by ascending frame.
+    ctc_tracks : dict[int, dict[str, int]]
+        A dictionary containing the CTC tracks of the lineage.
+    node_to_parent_track : dict[int, int]
+        A dictionary mapping the nodes to their parent CTC track.
+    current_track_label : int
+        The current track label.
+
+    Returns
+    -------
+    int
+        The updated current track label.
+    """
     track = {
         "B": lineage.nodes[sorted_nodes[0]]["frame"],
         "E": lineage.nodes[sorted_nodes[-1]]["frame"],
@@ -106,25 +116,25 @@ def export_CTC(model, ctc_file_out):
         node_to_parent_track = {}
         for cc in lin.get_cell_cycles(keep_incomplete_cell_cycles=True):
             # print(cc)
-            sorted_nodes, frames = sort_nodes_by_frame(lin, cc)
+            sorted_nodes = sort_nodes_by_frame(lin, cc)
             gaps = _find_gaps(lin, sorted_nodes)
             if gaps:
                 print("Gaps found in cell cycle:", cc)
                 print("gaps:", gaps)
-                start_i = 0
+                track_start_i = 0
                 for gap in gaps:
-                    end_i = sorted_nodes.index(gap[0])
+                    track_end_i = sorted_nodes.index(gap[0])
                     current_track_label = _add_track(
                         lin,
-                        sorted_nodes[start_i : end_i + 1],
+                        sorted_nodes[track_start_i : track_end_i + 1],
                         ctc_tracks,
                         node_to_parent_track,
                         current_track_label,
                     )
-                    start_i = sorted_nodes.index(gap[1])
+                    track_start_i = sorted_nodes.index(gap[1])
                 current_track_label = _add_track(
                     lin,
-                    sorted_nodes[start_i:],
+                    sorted_nodes[track_start_i:],
                     ctc_tracks,
                     node_to_parent_track,
                     current_track_label,
@@ -139,9 +149,7 @@ def export_CTC(model, ctc_file_out):
                 )
 
         for track_label, track_info in ctc_tracks.items():
-            # print(track_label, track_info)
             parent_nodes = list(lin.predecessors(track_info["B_node"]))
-            # Pycellin and CTC do not support multiple parents.
             assert_msg = (
                 f"Node {track_info['B_node']} has more than 1 parent node "
                 f"({len(parent_nodes)} nodes) in lineage of ID "
@@ -156,9 +164,6 @@ def export_CTC(model, ctc_file_out):
             print(
                 f"{track_label} {track_info['B']} {track_info['E']} {track_info['P']}"
             )
-
-        # print("ctc_tracks", ctc_tracks)
-        # print()
 
 
 if __name__ == "__main__":
