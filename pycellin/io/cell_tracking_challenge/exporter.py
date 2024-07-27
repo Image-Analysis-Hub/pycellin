b2982637a5d45fe4a03ee06910cda6415a7b3ccb
Mini-Miette <laura.xenard@protonmail.com>
2024-07-27 13:50:53 +0200

    Refactoring

--- pycellin/io/cell_tracking_challenge/exporter.py
+++ pycellin/io/cell_tracking_challenge/exporter.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 from pycellin.classes.lineage import CellLineage
+from pycellin.io.cell_tracking_challenge.loader import load_CTC_file
 from pycellin.io.trackmate.loader import load_TrackMate_XML
+
+# TODO: need to extensively test this.
+
+# refactor
+# actual writing to file
 
 
 def sort_nodes_by_frame(
@@ -102,90 +107,102 @@ def _add_track(
     return current_track_label + 1
 
 
-def export_CTC(model, ctc_file_out):
+def _build_CTC_tracks(
+    lineage: CellLineage,
+    ctc_tracks: dict[int, dict[str, int]],
+    node_to_parent_track: dict[int, int],
+    current_track_label: int,
+) -> int:
+    if len(lineage) == 1:
+        current_track_label = _add_track(
+            lineage,
+            list(lineage.nodes),
+            ctc_tracks,
+            node_to_parent_track,
+            current_track_label,
+        )
+    else:
+        for cc in lineage.get_cell_cycles(keep_incomplete_cell_cycles=True):
+            sorted_nodes = sort_nodes_by_frame(lineage, cc)
+            gaps = _find_gaps(lineage, sorted_nodes)
+            if gaps:
+                print("Gaps found in cell cycle:", cc)
+                print("gaps:", gaps)
+                track_start_i = 0
+                for gap in gaps:
+                    track_end_i = sorted_nodes.index(gap[0])
+                    current_track_label = _add_track(
+                        lineage,
+                        sorted_nodes[track_start_i : track_end_i + 1],
+                        ctc_tracks,
+                        node_to_parent_track,
+                        current_track_label,
+                    )
+                    track_start_i = sorted_nodes.index(gap[1])
+                current_track_label = _add_track(
+                    lineage,
+                    sorted_nodes[track_start_i:],
+                    ctc_tracks,
+                    node_to_parent_track,
+                    current_track_label,
+                )
+            else:
+                current_track_label = _add_track(
+                    lineage,
+                    sorted_nodes,
+                    ctc_tracks,
+                    node_to_parent_track,
+                    current_track_label,
+                )
+    return current_track_label
 
-    # L B E P
 
+def _add_parent_track(
+    lineage: CellLineage,
+    track_info: dict[str, int],
+    node_to_parent_track: dict[int, int],
+):
+    parent_nodes = list(lineage.predecessors(track_info["B_node"]))
+    assert_msg = (
+        f"Node {track_info['B_node']} has more than 1 parent node "
+        f"({len(parent_nodes)} nodes) in lineage of ID "
+        f"{lineage.graph['lineage_ID']}. Incorrect lineage topology: "
+        f"Pycellin and CTC do not support merge events."
+    )
+    assert len(parent_nodes) <= 1, assert_msg
+    if parent_nodes:
+        track_info["P"] = node_to_parent_track[parent_nodes[0]]
+    else:
+        track_info["P"] = 0
+
+
+def export_CTC_file(model, ctc_file_out):
     lineages = model.coredata.data
-    # Removing one-node lineages.
-    # TODO: actually CTC can deal with one-node lineage, so I need to deal
-    # with this kind of special cases when everything else will work.
     lineages = [lineage for lineage in lineages.values()]
     current_track_label = 1  # 0 is kept for no parent track
+    tracks_to_write = []
     for lin in lineages:
         print(lin)
         ctc_tracks = {}
         node_to_parent_track = {}
-
-        if len(lin) == 1:
-            # print("ONE_NODE LINEAGE")
-            current_track_label = _add_track(
-                lin,
-                list(lin.nodes),
-                ctc_tracks,
-                node_to_parent_track,
-                current_track_label,
-            )
-        else:
-            for cc in lin.get_cell_cycles(keep_incomplete_cell_cycles=True):
-                # print(cc)
-                sorted_nodes = sort_nodes_by_frame(lin, cc)
-                print(sorted_nodes)
-                gaps = _find_gaps(lin, sorted_nodes)
-                if gaps:
-                    print("Gaps found in cell cycle:", cc)
-                    print("gaps:", gaps)
-                    track_start_i = 0
-                    for gap in gaps:
-                        track_end_i = sorted_nodes.index(gap[0])
-                        current_track_label = _add_track(
-                            lin,
-                            sorted_nodes[track_start_i : track_end_i + 1],
-                            ctc_tracks,
-                            node_to_parent_track,
-                            current_track_label,
-                        )
-                        track_start_i = sorted_nodes.index(gap[1])
-                    current_track_label = _add_track(
-                        lin,
-                        sorted_nodes[track_start_i:],
-                        ctc_tracks,
-                        node_to_parent_track,
-                        current_track_label,
-                    )
-                else:
-                    current_track_label = _add_track(
-                        lin,
-                        sorted_nodes,
-                        ctc_tracks,
-                        node_to_parent_track,
-                        current_track_label,
-                    )
-
+        current_track_label = _build_CTC_tracks(
+            lin, ctc_tracks, node_to_parent_track, current_track_label
+        )
         print(ctc_tracks)
 
         for track_label, track_info in ctc_tracks.items():
-            parent_nodes = list(lin.predecessors(track_info["B_node"]))
-            assert_msg = (
-                f"Node {track_info['B_node']} has more than 1 parent node "
-                f"({len(parent_nodes)} nodes) in lineage of ID "
-                f"{lin.graph['lineage_ID']}. Incorrect lineage topology: "
-                f"Pycellin and CTC do not support merge events."
-            )
-            assert len(parent_nodes) <= 1, assert_msg
-            if parent_nodes:
-                track_info["P"] = node_to_parent_track[parent_nodes[0]]
-            else:
-                track_info["P"] = 0
-            print(
-                f"{track_label} {track_info['B']} {track_info['E']} {track_info['P']}"
-            )
+            _add_parent_track(lin, track_info, node_to_parent_track)
+            txt = f"{track_label} {track_info['B']} {track_info['E']} {track_info['P']}"
+            tracks_to_write.append(txt)
 
 
 if __name__ == "__main__":
 
     xml_in = "sample_data/FakeTracks.xml"
+    ctc_in = "C:/Users/haiba/Documents/01_RES/res_track.txt"
     ctc_out = "sample_data/FakeTracks_exported_CTC.xml"
 
     model = load_TrackMate_XML(xml_in, keep_all_spots=True, keep_all_tracks=True)
-    export_CTC(model, ctc_out)
+    # model = load_CTC_file(ctc_in) => FIXME
+
+    export_CTC_file(model, ctc_out)
