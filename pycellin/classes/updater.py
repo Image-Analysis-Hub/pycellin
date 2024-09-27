6a8ad6fd559e6063f019eb5e35166c8448d4c59f
lxenard <laura.xenard@pasteur.fr>
2024-09-27 15:50:44 +0200

    Add TODOs

--- pycellin/classes/updater.py
+++ pycellin/classes/updater.py
@@ -17,18 +17,26 @@ class ModelUpdater:
         # In this case I need to be able to modify the content of the collection
         self._added_cells = set()  # set of Cell()
         self._removed_cells = set()
-        self._added_links = set()
+        self._added_links = set()  # set of Link()
         self._removed_links = set()
 
     def _update_cells(self, dict_feat: dict[str, Any], data: Data):
         for cell in self._added_cells:
             print(f"Cell {cell.cell_ID} from lineage {cell.lineage_ID}:")
             print(data.cell_data[cell.lineage_ID].nodes[cell.cell_ID])
-            # TODO:  implement
+            # TODO: implement
             # How do I know which features need to be updated?
             # And how to compute custom features?
             # On regarde si dans les features déclarées, il y a des features de pycellin
-            # qui n'ont pas été éjà calculées
+            # qui n'ont pas été déjà calculées
+
+    # TODO: need to check somewhere that no fusion were created when
+    # modifying the graphs
+    # => maybe better in Model?
+    # Implement a higher level function in Modelbut need to check for fusion
+    # every time an edge is added
+    # TODO: also need to check if there are lone nodes in the graph, or a graph with
+    # several connected components, or a graph with no nodes.
 
     # def _update_edges(self):
     #     pass
