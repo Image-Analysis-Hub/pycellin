7503be721a239bad0bf3518f7bd8fb02aa7964b7
lxenard <laura.xenard@pasteur.fr>
2024-09-27 18:45:20 +0200

    New Exceptions inheriting from LineageStructureError

--- pycellin/classes/exceptions.py
+++ pycellin/classes/exceptions.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# TODO: add a WWarning when a feature is not present across all cells,
+# links, or lineages?
+
 
 class LineageStructureError(Exception):
     """
-    Exception raised for errors in the lineage structure.
-
-    For now, the only use-case I have for this exception
-    is when one of the node has several parents,
-    i.e. there are fusions in the lineage.
+    Raised when an incorrect lineage structure is detected.
     """
 
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
+
+
+class FusionError(LineageStructureError):
+    """
+    Raised when a fusion event is detected in the lineage structure.
+
+    A fusion event happens when a node has more than one parent,
+    i. e. an in_degree greater than 1.
+    """
+
+    pass
+
+
+class TimeFlowError(LineageStructureError):
+    """
+    Raised when a time flow error is detected in the lineage structure.
+
+    In a lineage graph, time flows from the root of the graph to the leaves.
+    As a result, a node should always have a time value greater than its parent.
+    """
+
+    pass
