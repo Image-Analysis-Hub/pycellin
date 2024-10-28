b8c00ccad31c909222d127c2c73a3ba166534afc
lxenard <laura.xenard@pasteur.fr>
2024-10-28 18:47:38 +0100

    Implementation of all abstract classes of FeatureCalculator

--- pycellin/classes/feature_calculator.py
+++ pycellin/classes/feature_calculator.py
@@ -2,30 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from abc import ABC, abstractmethod
+from typing import Any
 
 from pycellin.classes.data import Data
 from pycellin.classes.feature import Feature
 from pycellin.classes.lineage import Lineage
 
-
-# Make an interface that specializes depending on:
-# - the different types of objects: cells, links, lineages... (cell cycles?)
-# - if the feature is local or global
-# Local means that the feature only needs data from the current object
-# to be computed.
-# Global means that the feature needs data from other objects to be computed.
-# For now feature type (node, edge, lineage) is not stored in the feature object.
-# It probably should be. Same with local / global?
 # In FeaturesDeclaration, maybe all the features should be stored in the same
 # dictionary, with getters to access the features of a specific type?
-
-# Common interface to compute a feature value:
-# takes an object and returns a value
-# In case of global features, I will need the whole model or at least the current
-# lineage to compute the feature value. Will this not create a circular dependency?
-
-# Maybe first separate into node, edge, lineage, then do the local / global separation.
-# Because then the apply_on_all function can be defined in the middle level classes.
 
 
 class FeatureCalculator(ABC):
@@ -34,10 +18,7 @@ class FeatureCalculator(ABC):
     """
 
     @abstractmethod
-    def compute(
-        self,
-        data: Data,
-    ) -> None:
+    def compute(self, *args, **kwargs) -> Any:
         """
         Compute the value of a feature.
 
@@ -47,96 +28,189 @@ class FeatureCalculator(ABC):
         """
         pass
 
+    @abstractmethod
+    def add_to_lineages(self, feature: Feature, data: Data) -> None:
+        pass
 
-class NodeFeatureCalculator(FeatureCalculator):
+    @abstractmethod
+    def _add_feature_to_lineage(self, feat_name: str, lineage: Lineage) -> None:
+        pass
+
+
+class LocalFeatureCalculator(FeatureCalculator):
     """
-    Class to compute node feature values.
+    Abstract class to compute local feature values and add them to lineages.
+
+    Local features are features that only need data from the current object
+    to be computed.
+    Examples:
+    - cell area (node feature) only need data from the cell itself
+      (coordinates of boundary points) ;
+    - cell speed (edge feature) only need data from the edge itself
+      (time and space location of the two nodes) ;
+    - lineage duration (lineage feature) only need data from the lineage itself
+      (number of timepoints).
     """
 
     @abstractmethod
-    def compute_for_one_node(
-        self,
-        node: int,
-        lineage: Lineage,
-    ) -> None:
-        """
-        Compute the value of a feature for a single node.
-
-        Parameters
-        ----------
-        node : str
-            The name of the node.
-        lineage : Lineage
-            The lineage the node belongs to.
-        time_step : int
-            The time step the node is at.
-        """
+    def compute(self, lineage: Lineage, *args, **kwargs) -> Any:
         pass
 
-    def compute_and_add(
-        self,
-        feature: Feature,
-        data: Data,
-    ) -> None:
-        """
-        Compute the value of a node feature.
+    def add_to_lineages(self, feature: Feature, data: Data) -> None:
+        feat_name = feature.name
+        lin_type = feature.lineage_type
+        if lin_type == "CellLineage":
+            lineages = data.cell_data.values()
+        else:
+            lineages = data.cycle_data.values()
 
-        Parameters
-        ----------
+        for lin in lineages:
+            self._add_feature_to_lineage(lin, feat_name)
 
-        """
-        for lin in data.cell_data.values():
-            for node in lin.nodes:
-                lin.nodes[node]["feature.name"] = self.compute_for_one_node(node, lin)
+    @abstractmethod
+    def _add_feature_to_lineage(self, feat_name: str, lineage: Lineage) -> None:
+        pass
 
 
-class FeatureCalculatorFactory:
+class NodeLocalFeatureCalculator(LocalFeatureCalculator):
+
+    @abstractmethod
+    def compute(self, lineage: Lineage, noi: int) -> Any:
+        pass
+
+    def _add_feature_to_lineage(self, feat_name: str, lineage: Lineage) -> None:
+        for noi in lineage.nodes:
+            lineage.nodes[noi][feat_name] = self.compute(lineage, noi)
+
+
+class EdgeLocalFeatureCalculator(LocalFeatureCalculator):
+
+    @abstractmethod
+    def compute(self, lineage: Lineage, edge: tuple[int, int]) -> Any:
+        pass
+
+    def _add_feature_to_lineage(self, feat_name: str, lineage: Lineage) -> None:
+        for edge in lineage.edges:
+            lineage.edges[edge][feat_name] = self.compute(lineage, edge)
+
+
+class LineageLocalFeatureCalculator(LocalFeatureCalculator):
+
+    @abstractmethod
+    def compute(self, lineage: Lineage) -> Any:
+        pass
+
+    def _add_feature_to_lineage(self, feat_name: str, lineage: Lineage) -> None:
+        lineage.graph[feat_name] = self.compute(lineage)
+
+
+class GlobalFeatureCalculator(FeatureCalculator):
     """
-    Factory class to create feature calculators.
+    Abstract class to compute global feature values and add them to lineages.
+
+    Global features are features that need data from other objects to be computed.
+    Examples:
+    - cell age (node feature) needs data from all its ancestor cells in the lineage ;
+    - TODO: edge feature, find relevant example?
+    - TODO: lineage feature, find relevant example?
     """
 
-    def __init__(self) -> None:
-        self._calculators = {}
+    @abstractmethod
+    def compute(self, data: Data, lineage: Lineage, *args, **kwargs) -> Any:
+        pass
 
-    def register_calculator(
-        self, feature: Feature, calculator: FeatureCalculator
+    def add_to_lineages(self, feature: Feature, data: Data) -> None:
+        feat_name = feature.name
+        lin_type = feature.lineage_type
+        if lin_type == "CellLineage":
+            lineages = data.cell_data.values()
+        else:
+            lineages = data.cycle_data.values()
+
+        for lin in lineages:
+            self._add_feature_to_lineage(feat_name, lin, data)
+
+    @abstractmethod
+    def _add_feature_to_lineage(self, feat_name: str, lineage: Lineage) -> None:
+        pass
+
+
+class NodeGlobalFeatureCalculator(GlobalFeatureCalculator):
+
+    @abstractmethod
+    def compute(self, data: Data, lineage: Lineage, noi: int) -> Any:
+        pass
+
+    def _add_feature_to_lineage(
+        self, feat_name: str, lineage: Lineage, data: Data
     ) -> None:
-        """
-        Register a feature calculator.
+        for noi in lineage.nodes:
+            lineage.nodes[noi][feat_name] = self.compute(noi, lineage, data)
 
-        Parameters
-        ----------
-        feature : Feature
-            The feature to register the calculator for.
-        calculator : FeatureCalculator
-            The calculator to register.
-        """
-        self._calculators[feature] = calculator
 
-    def get_calculator(self, feature: Feature) -> FeatureCalculator:
-        """
-        Get the calculator for a feature.
+class EdgeGlobalFeatureCalculator(GlobalFeatureCalculator):
 
-        Parameters
-        ----------
-        feature : Feature
-            The feature to get the calculator for.
+    @abstractmethod
+    def compute(self, data: Data, lineage: Lineage, edge: tuple[int, int]) -> Any:
+        pass
 
-        Returns
-        -------
-        FeatureCalculator
-            The calculator for the feature.
-        """
-        calculator = self._calculators.get(feature)
-        if calculator is None:
-            raise ValueError(f"No calculator registered for feature {feature}.")
-        return calculator
-    
+    def _add_feature_to_lineage(
+        self, feat_name: str, lineage: Lineage, data: Data
+    ) -> None:
+        for edge in lineage.edges:
+            lineage.edges[edge][feat_name] = self.compute(edge, lineage, data)
 
-# class Calculator:
-#     def __init__(self, factory: FeatureCalculatorFactory):
-#         self.factory = factory
 
-#     def calculate(self, operation_name: str, a: float, b: float) -> float:
-#         operation = self.factory.create_operation(operation_name)
-#         return operation.compute(a, b)
+class LineageGlobalFeatureCalculator(GlobalFeatureCalculator):
+
+    @abstractmethod
+    def compute(self, data: Data, lineage: Lineage) -> Any:
+        pass
+
+    def _add_feature_to_lineage(
+        self, feat_name: str, lineage: Lineage, data: Data
+    ) -> None:
+        lineage.graph[feat_name] = self.compute(lineage, data)
+
+
+# class FeatureCalculatorFactory:
+#     """
+#     Factory class to create feature calculators.
+#     """
+
+#     def __init__(self) -> None:
+#         self._calculators = {}
+
+#     def register_calculator(
+#         self, feature: Feature, calculator: FeatureCalculator
+#     ) -> None:
+#         """
+#         Register a feature calculator.
+
+#         Parameters
+#         ----------
+#         feature : Feature
+#             The feature to register the calculator for.
+#         calculator : FeatureCalculator
+#             The calculator to register.
+#         """
+#         self._calculators[feature] = calculator
+
+#     def get_calculator(self, feature: Feature) -> FeatureCalculator:
+#         """
+#         Get the calculator for a feature.
+
+#         Parameters
+#         ----------
+#         feature : Feature
+#             The feature to get the calculator for.
+
+#         Returns
+#         -------
+#         FeatureCalculator
+#             The calculator for the feature.
+#         """
+#         calculator = self._calculators.get(feature)
+#         if calculator is None:
+#             raise ValueError(f"No calculator registered for feature {feature}.")
+#         return calculator
