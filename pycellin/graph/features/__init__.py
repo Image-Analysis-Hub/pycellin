630e4335683c80eb2fb9947960a6de7a556574b5
Laura Xénard <laura.xenard@pasteur.fr>
2024-11-07 10:52:54 +0100

    Update imports

--- pycellin/graph/features/__init__.py
+++ pycellin/graph/features/__init__.py
@@ -1,3 +1,6 @@
-from .morphology import get_width_and_length, get_area_increment
+# TODO: do I really need to import stuff from these modules?
+# Because users are not supposed to use these classes directly.
+
+from .morphology import *
 from .tracking import *
 from .utils import *
