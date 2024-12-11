aaf103aa0b1880822256eca46e9055990335cb25
lxenard <laura.xenard@pasteur.fr>
2024-12-11 13:50:23 +0100

    Move get_pycellin_cell_lineage_features() and cycle lin equivalent to features.utils.py

--- pycellin/classes/model.py
+++ pycellin/classes/model.py
@@ -165,24 +165,6 @@ class Model:
         """
         return self.metadata["time_step"]
 
-    def get_pycellin_cell_lineage_features(self) -> dict[str, str]:
-        """
-        Return the Pycellin features that can be computed on cell lineages.
-
-        Returns
-        -------
-        dict[str, str]
-            Dictionary of features of the cell lineages,
-            with features name as keys and features description as values.
-        """
-        cell_lineage_feats = {
-            "absolute_age": "Age of the cell since the beginning of the lineage",
-            "relative_age": (
-                "Age of the cell since the beginning of the current cell cycle"
-            ),
-        }
-        return cell_lineage_feats
-
     def get_cell_lineage_features(self):
         """
         Return the cell lineages features present in the model.
@@ -206,28 +188,6 @@ class Model:
                 ]
             )
         return cell_lineage_feats
-
-    def get_pycellin_cycle_lineage_features(self) -> dict[str, str]:
-        """
-        Return the Pycellin features that can be computed on cycle lineages.
-
-        Returns
-        -------
-        dict[str, str]
-            Dictionary of features of the cycle lineages,
-            with features name as keys and features description as values.
-        """
-        cycle_lineage_feats = {
-            "cell_cycle_completeness": (
-                "Completeness of the cell cycle, "
-                "i.e. does it start and end with a division"
-            ),
-            "division_time": (
-                "Time elapsed between the birth of a cell and its division"
-            ),
-            "division_rate": "Number of divisions per time unit",
-        }
-        return cycle_lineage_feats
 
     def get_cycle_lineage_features(self):
         """
