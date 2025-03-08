a049e58b792342f60e6dff95e98c6c52b7a7635a
Laura Xénard <laura.xenard@pasteur.fr>
2025-03-08 17:21:01 +0100

    When updating, remove empty lineages and split lineages when unconnected components

--- pycellin/classes/updater.py
+++ pycellin/classes/updater.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import networkx as nx
 
 from pycellin.classes import Data
 from pycellin.classes.feature_calculator import FeatureCalculator
+from pycellin.classes.lineage import CellLineage
 
 
 class ModelUpdater:
@@ -126,16 +128,61 @@ class ModelUpdater:
                 lineages_to_enrich=self._added_lineages | self._modified_lineages,
             )
 
-        # TODO: when updating:
-        # - empty lineages (no nodes nor links) should be removed
-        # - lineages with several connected components should be split
+        # Cleaning up the model.
+        # Remove empty lineages.
+        for lin_ID in (
+            self._added_lineages | self._modified_lineages
+        ) - self._removed_lineages:
+            print(lin_ID)
+            if len(data.cell_data[lin_ID]) == 0:
+                del data.cell_data[lin_ID]
+                self._removed_lineages.add(lin_ID)
+        # Split lineages with several connected components.
+        for lin in data.cell_data.values():
+            splitted_lins = [
+                CellLineage(lin.subgraph(c).copy())
+                for c in nx.weakly_connected_components(lin)
+            ]
+        # The largest lineage is considered to be the original one
+        # and will keep its lineage ID.
+        largest_lin = CellLineage()
+        for lin in splitted_lins:
+            if len(lin) > len(largest_lin):
+                largest_lin = lin
+        self._modified_lineages.add(largest_lin.graph["lineage_ID"])
+        splitted_lins.remove(largest_lin)
+        # The other lineages are considered as new lineages.
+        for lin in splitted_lins:
+            if len(lin) == 1:
+                # ID of a one-node lineage is minus the ID of the node.
+                new_lin_ID = -list(lin.nodes())[0]
+                if new_lin_ID in data.cell_data:
+                    # ID is already taken, so we change the ID of the node.
+                    new_cell_ID = max(data.cell_data.keys()) + 1
+                    cell_feats = lin._remove_cell(-new_lin_ID)
+                    frame = cell_feats["frame"].pop()
+                    assert len(lin) == 0
+                    lin._add_cell(new_cell_ID, frame, cell_feats)
+                    self._added_cells.add(new_cell_ID, -new_cell_ID)
+                    lin.graph["lineage_ID"] = -new_cell_ID
+                    data.cell_data[-new_cell_ID] = lin
+                    self._added_lineages.add(-new_cell_ID)
+                else:
+                    lin.graph["lineage_ID"] = new_lin_ID
+                    data.cell_data[new_lin_ID] = lin
+                    self._added_lineages.add(new_lin_ID)
+            else:
+                new_lin_ID = max(data.cell_data.keys()) + 1
+                lin.graph["lineage_ID"] = new_lin_ID
+                data.cell_data[new_lin_ID] = lin
+                self._added_lineages.add(new_lin_ID)
 
         # In case of modifications in the structure of some cell lineages,
         # we need to recompute the cycle lineages and their features.
         # TODO: optimize so we don't have to recompute EVERYTHING for cycle lineages?
         for lin_ID in (
-            (self._modified_lineages | self._added_lineages) - self._removed_lineages
-        ):
+            self._modified_lineages | self._added_lineages
+        ) - self._removed_lineages:
             if data.cycle_data is not None:
                 data.cycle_data[lin_ID] = data._compute_cycle_lineage(lin_ID)
         for lin_ID in self._removed_lineages:
