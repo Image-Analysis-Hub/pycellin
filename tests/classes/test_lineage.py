b79ccb0d15e31db1960119b39eeeb4e8f7e0ed41
Laura Xénard <laura.xenard@pasteur.fr>
2025-03-14 19:01:26 +0100

    Update fixtures, update get_root() tests, add get_leaves() tests

--- tests/classes/test_lineage.py
+++ tests/classes/test_lineage.py
@@ -11,20 +11,21 @@ from pycellin.classes import CellLineage, CycleLineage
 # CellLineage fixtures ########################################################
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def empty_cell_lin():
     return CellLineage()
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def one_node_cell_lin():
     lineage = CellLineage()
     lineage.add_node(1)
     return lineage
 
 
-@pytest.fixture(scope="module")
-def cell_lin1():
+@pytest.fixture
+def cell_lin():
+    # Nothing special, just a lineage.
     lineage = CellLineage()
     lineage.add_nodes_from(range(1, 13))
     lineage.add_edges_from(
@@ -45,30 +46,16 @@ def cell_lin1():
     return lineage
 
 
-@pytest.fixture(scope="module")
-def cell_lin2():
+@pytest.fixture
+def cell_lin_div_root(cell_lin):
     # The root is a division.
-    lineage = CellLineage()
-    lineage.add_nodes_from(range(1, 12))
-    lineage.add_edges_from(
-        [
-            (1, 2),
-            (2, 3),
-            (3, 4),
-            (4, 5),
-            (3, 6),
-            (6, 7),
-            (7, 8),
-            (7, 9),
-            (1, 10),
-            (10, 11),
-        ]
-    )
-    return lineage
+    cell_lin.add_node(13)
+    cell_lin.add_edge(1, 13)
+    return cell_lin
 
 
-@pytest.fixture(scope="module")
-def cell_lin3():
+@pytest.fixture
+def cell_lin_successive_divs_and_root():
     # Successive divisions and root division.
     lineage = CellLineage()
     lineage.add_nodes_from(range(2, 12))
@@ -88,31 +75,87 @@ def cell_lin3():
     return lineage
 
 
+@pytest.fixture
+def cell_lin_triple_div(cell_lin):
+    # Triple division.
+    cell_lin.add_nodes_from([13, 14])
+    cell_lin.add_edges_from([(4, 13), (13, 14)])
+    return cell_lin
+
+
+@pytest.fixture
+def cell_lin_unconnected_node(cell_lin):
+    cell_lin.add_node(13)
+    return cell_lin
+
+
+@pytest.fixture
+def cell_lin_unconnected_component(cell_lin):
+    cell_lin.add_nodes_from([13, 14])
+    cell_lin.add_edges_from([(13, 14)])
+    return cell_lin
+
+
 # get_root() ##################################################################
 
 
-def test_get_root_empty_lin():
-    lineage = CellLineage()
-    assert lineage.get_root() == []
-    assert lineage.get_root(ignore_lone_nodes=True) == []
+def test_get_normal_lin(cell_lin):
+    assert cell_lin.get_root() == 1
+    assert cell_lin.get_root(ignore_lone_nodes=True) == 1
 
 
-def test_get_root_single_node():
-    lineage = CellLineage()
-    lineage.add_node(1)
-    assert lineage.get_root() == 1
-    assert lineage.get_root(ignore_lone_nodes=True) == []
+def test_get_root_empty_lin(empty_cell_lin):
+    assert empty_cell_lin.get_root() == []
+    assert empty_cell_lin.get_root(ignore_lone_nodes=True) == []
 
 
-def test_get_root_unconnected_node(cell_lin1):
-    cell_lin1.add_node(13)
-    assert cell_lin1.get_root() == [1, 13]
-    assert cell_lin1.get_root(ignore_lone_nodes=True) == 1
+def test_get_root_single_node(one_node_cell_lin):
+    assert one_node_cell_lin.get_root() == 1
+    assert one_node_cell_lin.get_root(ignore_lone_nodes=True) == []
 
 
-def test_get_root_unconnected_component(cell_lin1):
-    cell_lin1.add_nodes_from([13, 14])
-    cell_lin1.add_edges_from([(13, 14)])
-    assert cell_lin1.get_root() == [1, 13]
-    assert cell_lin1.get_root(ignore_lone_nodes=True) == [1, 13]
-    
+def test_get_root_div_root(cell_lin_div_root):
+    assert cell_lin_div_root.get_root() == 1
+    assert cell_lin_div_root.get_root(ignore_lone_nodes=True) == 1
+
+
+def test_get_root_unconnected_node(cell_lin_unconnected_node):
+    assert cell_lin_unconnected_node.get_root() == [1, 13]
+    assert cell_lin_unconnected_node.get_root(ignore_lone_nodes=True) == 1
+
+
+def test_get_root_unconnected_component(cell_lin_unconnected_component):
+    assert cell_lin_unconnected_component.get_root() == [1, 13]
+    assert cell_lin_unconnected_component.get_root(ignore_lone_nodes=True) == [1, 13]
+
+
+# get_leaves() ################################################################
+
+
+def test_get_leaves_normal_lin(cell_lin):
+    assert cell_lin.get_leaves() == [6, 9, 10, 12]
+    assert cell_lin.get_leaves(ignore_lone_nodes=True) == [6, 9, 10, 12]
+
+
+def test_get_leaves_empty_lin(empty_cell_lin):
+    assert empty_cell_lin.get_leaves() == []
+    assert empty_cell_lin.get_leaves(ignore_lone_nodes=True) == []
+
+
+def test_get_leaves_single_node(one_node_cell_lin):
+    assert one_node_cell_lin.get_leaves() == [1]
+    assert one_node_cell_lin.get_leaves(ignore_lone_nodes=True) == []
+
+
+def test_get_leaves_unconnected_node(cell_lin_unconnected_node):
+    res = cell_lin_unconnected_node.get_leaves()
+    assert res == [6, 9, 10, 12, 13]
+    res = cell_lin_unconnected_node.get_leaves(ignore_lone_nodes=True)
+    assert res == [6, 9, 10, 12]
+
+
+def test_get_leaves_unconnected_component(cell_lin_unconnected_component):
+    res = cell_lin_unconnected_component.get_leaves()
+    assert res == [6, 9, 10, 12, 14]
+    res = cell_lin_unconnected_component.get_leaves(ignore_lone_nodes=True)
+    assert res == [6, 9, 10, 12, 14]
