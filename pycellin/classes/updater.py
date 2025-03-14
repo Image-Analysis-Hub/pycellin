5dc95305d7152cdb440902eecd317164e41c01ed
Laura Xénard <laura.xenard@pasteur.fr>
2025-03-14 18:59:47 +0100

    Fix _update()

--- pycellin/classes/updater.py
+++ pycellin/classes/updater.py
@@ -115,49 +115,55 @@ class ModelUpdater:
         for lin_ID in (
             self._added_lineages | self._modified_lineages
         ) - self._removed_lineages:
-            print(lin_ID)
             if len(data.cell_data[lin_ID]) == 0:
                 del data.cell_data[lin_ID]
                 self._removed_lineages.add(lin_ID)
-        # Split lineages with several connected components.
-        for lin in data.cell_data.values():
+
+        # Split lineages with several unconnected components.
+        lineages = list(data.cell_data.values())
+        for lin in lineages:
             splitted_lins = [
                 CellLineage(lin.subgraph(c).copy())
                 for c in nx.weakly_connected_components(lin)
             ]
-        # The largest lineage is considered to be the original one
-        # and will keep its lineage ID.
-        largest_lin = CellLineage()
-        for lin in splitted_lins:
-            if len(lin) > len(largest_lin):
-                largest_lin = lin
-        self._modified_lineages.add(largest_lin.graph["lineage_ID"])
-        splitted_lins.remove(largest_lin)
-        # The other lineages are considered as new lineages.
-        for lin in splitted_lins:
-            if len(lin) == 1:
-                # ID of a one-node lineage is minus the ID of the node.
-                new_lin_ID = -list(lin.nodes())[0]
-                if new_lin_ID in data.cell_data:
-                    # ID is already taken, so we change the ID of the node.
-                    new_cell_ID = max(data.cell_data.keys()) + 1
-                    cell_feats = lin._remove_cell(-new_lin_ID)
-                    frame = cell_feats["frame"].pop()
-                    assert len(lin) == 0
-                    lin._add_cell(new_cell_ID, frame, cell_feats)
-                    self._added_cells.add(new_cell_ID, -new_cell_ID)
-                    lin.graph["lineage_ID"] = -new_cell_ID
-                    data.cell_data[-new_cell_ID] = lin
-                    self._added_lineages.add(-new_cell_ID)
+            if len(splitted_lins) == 1:
+                continue
+            # The largest lineage is considered to be the original one
+            # and will keep its lineage ID.
+            largest_lin = CellLineage()
+            for lin in splitted_lins:
+                if len(lin) > len(largest_lin):
+                    largest_lin = lin
+            # We replace it in the data, otherwise the unsplitted lineage
+            # will be kept.
+            data.cell_data[largest_lin.graph["lineage_ID"]] = largest_lin
+            self._modified_lineages.add(largest_lin.graph["lineage_ID"])
+            splitted_lins.remove(largest_lin)
+            # The other lineages are considered as new lineages.
+            for lin in splitted_lins:
+                if len(lin) == 1:
+                    # ID of a one-node lineage is minus the ID of the node.
+                    new_lin_ID = -list(lin.nodes())[0]
+                    if new_lin_ID in data.cell_data:
+                        # ID is already taken, so we change the ID of the node.
+                        new_cell_ID = max(data.cell_data.keys()) + 1
+                        cell_feats = lin._remove_cell(-new_lin_ID)
+                        frame = cell_feats.pop("frame")
+                        assert len(lin) == 0
+                        lin._add_cell(new_cell_ID, frame, **cell_feats)
+                        self._added_cells.add(new_cell_ID)
+                        lin.graph["lineage_ID"] = -new_cell_ID
+                        data.cell_data[-new_cell_ID] = lin
+                        self._added_lineages.add(-new_cell_ID)
+                    else:
+                        lin.graph["lineage_ID"] = new_lin_ID
+                        data.cell_data[new_lin_ID] = lin
+                        self._added_lineages.add(new_lin_ID)
                 else:
+                    new_lin_ID = max(data.cell_data.keys()) + 1
                     lin.graph["lineage_ID"] = new_lin_ID
                     data.cell_data[new_lin_ID] = lin
                     self._added_lineages.add(new_lin_ID)
-            else:
-                new_lin_ID = max(data.cell_data.keys()) + 1
-                lin.graph["lineage_ID"] = new_lin_ID
-                data.cell_data[new_lin_ID] = lin
-                self._added_lineages.add(new_lin_ID)
 
         # In case of modifications in the structure of some cell lineages,
         # we need to recompute the cycle lineages and their features.
