381dacb716e8a24ff7e8f69849a59baae3e360e1
lxenard <laura.xenard@pasteur.fr>
2025-03-28 14:35:21 +0100

    Restoring epicure IO folder because apparently I messed up

---
+++ pycellin/io/epicure/exporter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pycellin.io.trackmate.loader import load_TrackMate_XML
+
+
+def export_EpiCure_pickle():
+    pass
+
+
+if __name__ == "__main__":
+    pass
