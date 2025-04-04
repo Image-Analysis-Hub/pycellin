5419315887750621dd0af0c6920fe3c9445f6784
lxenard <laura.xenard@pasteur.fr>
2025-04-04 12:19:23 +0200

    Trackpy IO stub

---
+++ pycellin/io/trackpy/exporter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+exporter.py
+
+This module is part of the Pycellin package.
+
+This module provides functions to export Pycellin models to trackpy tracking files.
+It includes a function to export a Pycellin model to a trackpy file and helper functions
+to build trackpy tracks from a lineage.
+
+References:
+- trackpy: D. B. Allan, T. Caswell, N. C. Keim, C. M. van der Weland R. W. Verweij,
+“soft-matter/trackpy: v0.6.4”. Zenodo, Jul. 10, 2024. doi: 10.5281/zenodo.12708864.
+- trackpy GitHub: https://github.com/soft-matter/trackpy
+"""
+
+import trackpy as tp
+
+
+if __name__ == "__main__":
+    pass
---
+++ pycellin/io/trackpy/loader.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+loader.py
+
+This module is part of the Pycellin package.
+
+This module provides functions to load and process trackpy data into Pycellin models.
+It includes a function to load a trackpy file into Pycellin model and helper functions
+to create metadata, features, and lineage graphs.
+
+References:
+- trackpy: D. B. Allan, T. Caswell, N. C. Keim, C. M. van der Weland R. W. Verweij,
+“soft-matter/trackpy: v0.6.4”. Zenodo, Jul. 10, 2024. doi: 10.5281/zenodo.12708864.
+- trackpy GitHub: https://github.com/soft-matter/trackpy
+"""
+
+import trackpy as tp
+
+
+if __name__ == "__main__":
+    pass
