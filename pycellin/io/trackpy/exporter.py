286fe344608edb6885e2754879c042e4e95e560d
lxenard <laura.xenard@pasteur.fr>
2025-04-15 14:29:28 +0200

    Functional trackpy exporter

--- pycellin/io/trackpy/exporter.py
+++ pycellin/io/trackpy/exporter.py
@@ -42,23 +42,67 @@ def export_trackpy_dataframe(model: Model) -> pd.DataFrame:
     pd.DataFrame
         A DataFrame containing trackpy formatted data.
     """
-    # We don't want to modify the original model.
-    model_copy = copy.deepcopy(model)
+    model_copy = copy.deepcopy(model)  # Don't want to modify the original model.
+
+    # We want to safekeep the original lineage IDs in the nodes of the model since
+    # we are going to rename and/or renumber them.
+    for lin_ID, lin in model_copy.data.cell_data.items():
+        for node in lin.nodes():
+            lin.nodes[node]["lineage_ID_Pycellin"] = lin_ID
+
     # Removal of division events.
     # We simply remove the edges involved in the divisions.
-    print("model before", model_copy)
     for lin in model_copy.get_cell_lineages():
-        print("before", lin)
         divs = lin.get_divisions()
-        div_edges = [lin.out_edges(div) for div in divs]
+        div_edges = [edge for div in divs for edge in lin.out_edges(div)]
         for edge in div_edges:
-            print("removing edge", edge)
             model_copy.remove_link(*edge, lin.graph["lineage_ID"])
-        print("after", lin)
     model_copy.update()
-    print("model after", model_copy)
 
-    df = pd.DataFrame()
+    # Trackpy might not like negative lineage IDs so we change them to positive ones.
+    one_node_lin_IDs = [
+        lin.graph["lineage_ID"]
+        for lin in model_copy.get_cell_lineages()
+        if lin.graph["lineage_ID"] < 0
+    ]
+    for lin_ID in one_node_lin_IDs:
+        lin = model_copy.get_cell_lineage_from_ID(lin_ID)
+        assert lin is not None
+        new_lin_ID = model_copy.get_next_available_lineage_ID()
+        # Update the lineage ID in the graph.
+        lin.graph["lineage_ID"] = new_lin_ID
+        # Update the lineage ID in the cell data.
+        model_copy.data.cell_data.pop(lin_ID)
+        model_copy.data.cell_data[new_lin_ID] = lin
+
+    # Creation of the trackpy DataFrame.
+    df = model_copy.to_cell_dataframe()
+    # We have to rename some columns to be compatible with trackpy.
+    if "particle" in df.columns:
+        # If we already have this column, it means the data is coming from
+        # trackpy, but it might not be up to date. Safer to remove it and
+        # rename it from "lineage_ID".
+        df.drop(columns=["particle"], inplace=True)
+    df.rename(columns={"lineage_ID": "particle"}, inplace=True)
+    df.rename(columns={"cell_x": "x"}, inplace=True)
+    df.rename(columns={"cell_y": "y"}, inplace=True)
+    if "cell_z" in df.columns:
+        df.rename(columns={"cell_z": "z"}, inplace=True)
+    if "ROI_coords" in df.columns:
+        # We need to remove the ROI_coords column.
+        df.drop(columns=["ROI_coords"], inplace=True)
+    # Reorder the columns to match trackpy format.
+    if "z" in df.columns:
+        dim_columns = ["z", "y", "x"]
+    else:
+        dim_columns = ["y", "x"]
+    df = df[
+        dim_columns
+        + [col for col in df.columns if col not in ["z", "y", "x", "frame", "particle"]]
+        + ["frame", "particle"]
+    ]
+    # Sort the rows.
+    df.sort_values(by=["particle", "frame"], inplace=True)
 
     return df
 
@@ -66,36 +110,21 @@ def export_trackpy_dataframe(model: Model) -> pd.DataFrame:
 if __name__ == "__main__":
     folder = "/mnt/data/Code/trackpy-examples-master/sample_data/"
     tracks = "FakeTracks_trackpy.pkl"
+    xml = "sample_data/Ecoli_growth_on_agar_pad.xml"
 
     df = pd.read_pickle(folder + tracks)
+    print(df.head())
 
-    from pycellin import load_trackpy_dataframe
+    from pycellin import load_trackpy_dataframe, load_TrackMate_XML
 
+    # model = load_TrackMate_XML(xml)
     model = load_trackpy_dataframe(df)
     for lin in model.get_cell_lineages():
         print(lin)
-    #     lin.plot()
-    # model.add_link(source_cell_ID=8, source_lineage_ID=0, target_cell_ID=9)
-
-    # model.add_link(
-    #     source_cell_ID=8, source_lineage_ID=0, target_cell_ID=10, target_lineage_ID=1
-    # )
-
-    # model.get_cell_lineage_from_ID(0)._add_link(
-    #     source_noi=8, target_noi=10, target_lineage=model.get_cell_lineage_from_ID(1)
-    # )
-
-    # model.get_cell_lineage_from_ID(1)._remove_link(10, 11)
-    # model.get_cell_lineage_from_ID(0)._add_link(
-    #     source_noi=9, target_noi=11, target_lineage=model.get_cell_lineage_from_ID(1)
-    # )
-
-    model.remove_link(10, 11, 1)
     model.add_link(
-        source_cell_ID=9, source_lineage_ID=0, target_cell_ID=11, target_lineage_ID=1
+        source_cell_ID=8, source_lineage_ID=0, target_cell_ID=10, target_lineage_ID=1
     )
     model.update()
-    # export_trackpy_dataframe(model)
-    for lin in model.get_cell_lineages():
-        print(lin)
-        lin.plot()
+
+    df = export_trackpy_dataframe(model)
+    print(df.head())
