54d6fba5186d84019411f33da7ac32d57f38f5ba
lxenard <laura.xenard@protonmail.com>
2025-09-22 16:02:43 +0200

    Add a get_pycellin_version() function to utils.py

--- pycellin/utils.py
+++ pycellin/utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import importlib.metadata
 from typing import get_args, get_origin, Literal
 
 
@@ -8,3 +9,11 @@ def check_literal_type(value, literal_type) -> bool:
     if get_origin(literal_type) is Literal:
         return value in get_args(literal_type)
     raise TypeError(f"{literal_type} is not a Literal type")
+
+
+def get_pycellin_version() -> str:
+    """Get pycellin version from package metadata"""
+    try:
+        return importlib.metadata.version("pycellin")
+    except importlib.metadata.PackageNotFoundError:
+        return "development"
